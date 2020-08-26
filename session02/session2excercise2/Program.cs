ef1a0f733decf1f41574c25d86a5eb461ea79635
DESKTOP-1FNV98K\Anton <nehvonen@protonmail.com>
2020-08-26 11:49:00 +0200

    excercise2

---
+++ session02/session2excercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace session2excercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to enter your name?");
+
+            var key = Console.ReadKey();
+
+            if( key.KeyChar == 'n')
+            return;
+
+            Console.WriteLine("Hello, enter your name?");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey(); 
+        }
+    }
+}
