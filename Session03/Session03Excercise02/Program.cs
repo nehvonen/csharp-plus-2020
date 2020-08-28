fd4a4a3dea652f56c4808c06cc20937a4b2b3fe6
DESKTOP-1FNV98K\Anton <nehvonen@protonmail.com>
2020-08-28 09:14:55 +0200

    Session03

---
+++ Session03/Session03Excercise02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Session03Excercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
+
+            var input = Console.ReadLine();
+
+            var inputData = input.Split(" , ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in inputData)
+            {
+                Console.WriteLine("Värdet är " + number);
+            }
+
+        }
+    }
+}
---
+++ Session03/Session3Excercise1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Session3Excercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var integer = new int[] { 1, 2, 3, 4 };
+            var name = nameof(integer);
+
+            for (var i = 0; i < integer.Length; i++)
+            {
+                
+                var value = integer[i];
+
+                Console.WriteLine($"Index {i} i arrayen {(nameof(integer))} har värdet: {value}");
+            }
+
+            var dowhileIndex = 0;
+            do
+            {
+                var value = integer[dowhileIndex];
+                Console.WriteLine($"Index do while {dowhileIndex} i arrayen {(nameof(integer))} har värdet: {value}");
+                dowhileIndex++;
+
+            }
+            while (dowhileIndex < integer.Length);
+
+            var whileIndex = 0;
+
+            while (whileIndex < integer.Length)
+            {
+                var value = integer[whileIndex]; 
+                
+                Console.WriteLine($"While Index {whileIndex} i arrayen {(nameof(integer))} har värdet: {value}");
+                whileIndex++;
+            }
+
+        }
+    }
+}
