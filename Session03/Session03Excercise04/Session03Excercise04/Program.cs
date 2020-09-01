9157a2e12387039e1a3eac7f1d5dd05f4681093c
DESKTOP-1FNV98K\Anton <nehvonen@protonmail.com>
2020-09-01 15:12:15 +0200

    Numbers 1.1 Duck Edition

--- Session03/Session03Excercise04/Session03Excercise04/Program.cs
+++ Session03/Session03Excercise04/Session03Excercise04/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//    _
+//  _(.)< *QUACK* 
+// (___)
+//
 
 namespace Session03Excercise04
 {
